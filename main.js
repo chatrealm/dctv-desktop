4d6eecfb2b233e831cecb8af91ee5396d27cb0c6
Alex Hanna <tinnvec@gmail.com>
2016-09-20 19:02:29 -0500

    Reset to base electron app

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ let win
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600})
-    
+
     // and load the index.html of the app.
-    win.loadURL(`file://${__dirname}/app/index.html`)
-    
+    win.loadURL(`file://${__dirname}/index.html`)
+
     // Open the DevTools.
-    win.webContents.openDevTools()
-    
+    // win.webContents.openDevTools()
+
     // Emitted when the window is closed.
     win.on('closed', () => {
         // Dereference the window object, usually you would store windows
