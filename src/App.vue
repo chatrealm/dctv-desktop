f825089f6b019c1768f767937c9af503f79b123b
Alex Hanna <tinnvec@gmail.com>
2016-09-21 13:03:23 -0500

    expose process versions

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
 <template>
     <div>
         <h1>{{ message }}</h1>
-        We are using node {{ process.versions.node }},
-        Chrome {{ process.versions.chrome }},
-        and Electron {{ process.versions.electron }}.
+        We are using node {{ versions.node }},
+        Chrome {{ versions.chrome }},
+        and Electron {{ versions.electron }}.
     </div>
 </template>
 
@@ -15,7 +15,8 @@
     export default { 
         data () {
             return {
-                message: "Hello World-Loader!"
+                message: "Hello World-Loader!",
+                versions: process.versions
             }
         }
     }
