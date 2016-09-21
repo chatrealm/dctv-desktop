00b4e4d984c75e4cdd0c29f068ce395fbeafa617
Alex Hanna <tinnvec@gmail.com>
2016-09-20 19:30:27 -0500

    Add use of component files with vue-loader

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 
+import App from './App.vue'
+
 new Vue({
-    el: '#app',
-    data: {
-        message: "Hello World!"
-    }
+    el: 'body',
+    components: { App }
 })
--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,25 @@ module.exports = {
     entry: './src/main.js',
     output: {
         path: './dist',
+        publicPath: 'dist/',
         filename: 'build.js'
     },
     module: {
         loaders: [
             {
-                test: /\.js/,
+                test: /\.js$/,
                 loader: 'babel',
                 exclude: /node_modules/
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue'
             }
         ]
+    },
+    vue: {
+        loaders: {
+            js: 'babel'
+        }
     }
 }
